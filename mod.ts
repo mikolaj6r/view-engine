459df6c80814d27eacfe50daa75bfb71dad88d4a
mikolaj6r <mikolaj6r@gmail.com>
2020-06-18 15:18:16 +0200

    more tests

--- mod.ts
+++ mod.ts
@@ -6,10 +6,8 @@ export { adapterFactory } from "./lib/adapterFactory.ts";
 export { viewEngine } from "./lib/viewEngine.ts"; //should be ok
 
 
-/*
+
 export { renderDenjuck } from "./lib/engines/denjuck.ts";
 
 export { renderEjs } from "./lib/engines/ejs.ts";
 export { renderHandlebars } from "./lib/engines/handlebars.ts";
-
-*/
